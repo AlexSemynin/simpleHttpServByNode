4dc59c965a0f3fe87a6f1ccba9fd8f6bbeb4746a
AlexSemynin <alex.semynin97@mail.ru>
2022-07-10 10:56:13 +0300

    change env getter from dotenv

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const env = require("dotenv");
 const http = require("http");
+env.config();
 
 console.log(process.pid);
-console.log(process.env.PORT); // см start in package.json. обязательно указать cross-env
+console.log(process.env.PORT);
+console.log(process.env.NODE_ENV);
